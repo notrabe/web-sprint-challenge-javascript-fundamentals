b10c4f0213e6c2f41ed5e96b2d1928b93fda9249
evan-barton <notrabe1011@gmail.com>
2020-07-24 13:46:59 -0400

    completed mvp tasks

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,21 @@ myFunction();
 
 // Explanation: 
 
+// because internal is within the scope of the function, i.e., it is in the outer function.
+
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+let count = 0
+
+function summation(num){
+  for(count = 0; count < num + 1; count ++){
+  }
+  return count + count
+}
+
+
+console.log(summation(4))
+
